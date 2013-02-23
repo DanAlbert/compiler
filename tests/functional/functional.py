b4bd6649038ba2cc10905686a094246c7ccbdd24
Ryley Herrington <herringr@flip1.engr.oregonstate.edu>
2013-02-22 19:47:36 -0800

    Added functional tests for parser.

--- tests/functional/functional.py
+++ tests/functional/functional.py
@@ -6,6 +6,18 @@ import subprocess
 import sys
 
 
+
+class Compiler_parser(object):
+    def __init__(self):
+        self.path = local_file('../../compiler')
+
+    def run(self, source):
+        proc = subprocess.Popen([self.path, '-s', source],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        return proc.communicate()
+
+
 class Compiler(object):
     def __init__(self):
         self.path = local_file('../../compiler')
@@ -16,7 +28,6 @@ class Compiler(object):
                                 stderr=subprocess.PIPE)
         return proc.communicate()
 
-
 def file_text(path):
     with open(path, 'r') as open_file:
         return open_file.read()
@@ -24,6 +35,24 @@ def file_text(path):
 def local_file(path):
     base = os.path.dirname(__file__)
     return os.path.join(base, path)
+
+
+def run_parse_tests():
+    errors = 0
+    golden = file_text(local_file('golden_parse.out'))
+    compiler = Compiler()
+    (out, err) = compiler.run(local_file('input2.lisp'))
+
+    if err:
+        print 'ERROR: stderr not empty'
+        print err
+        errors += 1
+    if out != golden:
+        print 'ERROR: output did not match expected'
+        print out
+        errors += 1
+
+    return errors
 
 def run_tests():
     errors = 0
@@ -44,8 +73,21 @@ def run_tests():
 
 if __name__ == '__main__':
    errors = run_tests()
+   parse_errors = run_parse_tests()
+
    if errors:
        print 'Total errors: %d' % errors
    else:
-       print 'All tests completed successfully.'
+       print 'Lexer tests completed successfully.'
+	
+   if parse_errors:
+       print 'Total errors: %d' % errors
+   else:
+       print 'Parser tests completed successfully.'
+
+   errors = errors + parse_errors
    sys.exit(errors)
+
+
+
+
